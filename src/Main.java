c09d0800473c1d488ad288b0ad358097b36c2524
Elisa Herrera Plasencia <elisaherreraplasencia@gmail.com>
2022-03-21 13:27:19 +0000

    Feature:Añadida nueva funcionanlidad al software

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Por favor,introduzca su nombre :");
+        String name= sc.nextLine();
+
+        System.out.println("Hola mundo"+ name);
     }
 }
