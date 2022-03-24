bbc0febfa433974049a67ade38f3c1d4eed301f6
Eliergato <101867022+Eliergato@users.noreply.github.com>
2022-03-24 10:37:23 +0000

    Chore:He añadido la documentacion y un .gitignore Durante el dia de hoy hemos actualizado el  readme

---
+++ README.md
@@ -0,0 +1,9 @@
+#Proyecto de dominicion mundial
+
+## Getting started
+
+### Para poder utilizar este proyecto dame un bitcoin
+
+# Quiero 
+
+[Acceso a la guia de instalacion](./docs/instalacionGuide.md)
--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,10 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
+
         System.out.println("Por favor,introduzca su nombre :");
         String name= sc.nextLine();
 
-        System.out.println("Hola mundo"+ name);
+        System.out.println("!Hola mundo!" + name);
     }
 }
